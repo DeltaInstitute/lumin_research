6e629453146e05dbb5113167dbfb05cdbe307ab1
colleenmcginnis <colleen.j.mcginnis@gmail.com>
2015-10-03 18:38:38 -0500

    adding css animations

--- _site/js/main.js
+++ _site/js/main.js
@@ -64,16 +64,15 @@ $('.researchQuestion').each(function () {
 // Recruitment animation
 //======================================================================================
 $(window).scroll(function () {
-$('nav').find('a').removeClass('active');
 $('.recruitment').each(function () {
     var imagePos = $(this).offset().top;
     var imageHeight = $(this).height();
     var topOfWindow = $(window).scrollTop();
 
     if (imagePos < topOfWindow + imageHeight && imagePos + imageHeight > topOfWindow) {
-        $('nav').find('#recruitmentNav').addClass('active');
+        $(this).addClass('drawBubble');
     } else {
-        $(this).removeClass("drawBulb");
+        $(this).removeClass("drawBubble");
     }
     });
 });
@@ -88,24 +87,9 @@ $('.code').each(function () {
     var topOfWindow = $(window).scrollTop();
 
     if (imagePos < topOfWindow + imageHeight && imagePos + imageHeight > topOfWindow) {
-        $('nav').find('#prototypeNav').addClass('active');
-        $(this).addClass('blink');
-        window.setTimeout(function () {
-        	$( ".cursor" ).find('line').attr( 'x1', '600');
-        	$( ".cursor" ).find('line').attr( 'x2', '600');
-    	}, 2800 );
-    	window.setTimeout(function () {
-        	$( ".cursor" ).find('line').attr( 'x1', '850');
-        	$( ".cursor" ).find('line').attr( 'x2', '850');
-    	}, 3300 );
-    	window.setTimeout(function () {
-        	$( ".cursor" ).find('line').attr( 'x1', '1100');
-        	$( ".cursor" ).find('line').attr( 'x2', '1100');
-    	}, 3800 );
+        $(this).addClass('cursorMove');
     } else {
-        $(this).removeClass('blink');
-        $( ".cursor" ).find('line').attr( 'x1', '225');
-        $( ".cursor" ).find('line').attr( 'x2', '225');
+        $(this).removeClass('cursorMove');
     }
 	});
 });
@@ -119,30 +103,9 @@ $('.envelope').each(function () {
     var topOfWindow = $(window).scrollTop();
 
     if (imagePos < topOfWindow + imageHeight && imagePos + imageHeight > topOfWindow) {
-        window.setTimeout(function () {
-        	$( ".envelopeOpen" ).css('opacity', 1);
-        	$( ".paperInside" ).css('opacity', 1);
-    	}, 1000 );
-    	window.setTimeout(function () {
-        	$( ".paperInside" ).find('rect').attr('height', 800).attr('y', 850);
-    	}, 1500 );
-    	window.setTimeout(function () {
-        	$( ".paperInside" ).find('rect').attr('height', 1200).attr('y', 450);
-    	}, 2000 );
-    	window.setTimeout(function () {
-        	$('.paperOutside').removeAttr('display');
-        	$( ".paperInside" ).find('rect').attr('height', 1200).attr('y', 50);
-    	}, 2500 );
-    	window.setTimeout(function () {
-        	$('.paperOutside').find('rect').attr('y', 550);
-        	$('.paperInside').find('rect').attr('y', 550);
-    	}, 3000 );
+        $('.envelope').addClass('open');
     } else {
-        $( ".envelopeOpen" ).css('opacity', 0);
-        $( ".paperInside" ).find('rect').attr('height', 400).attr('y', 1250);
-        $( ".paperInside" ).css('opacity', 0);
-        $('.paperOutside').attr('y', 550);
-        $('.paperOutside').attr('display', 'none');
+        $('.envelope').removeClass('open');
     }
 	
 	});
--- sections/03 finding-our-participants.md
+++ sections/03 finding-our-participants.md
@@ -4,6 +4,30 @@ title: Finding Our Participants
 bannerpath: fa fa-group
 ---
 <div class="recruitment">
+<svg x="0px" y="0px" viewBox="550 75 800 1100">
+	<g id="bubble">
+		<path fill="none" stroke="#FFFFFF" stroke-width="8" stroke-miterlimit="10" d="M879.9,824.3H196.1c-37.2,0-67.3-30.1-67.3-67.3
+		V364.8c0-36.4,35.1-70.1,70.1-70.1h795.4c38.9,0,70.4,31.5,70.4,70.4v382c0,48-33.2,77.8-75.4,77.8l23.2,121.7L879.9,824.3z"/>
+	</g>
+	<g id="joinUs">
+		<line id="joinUs9" fill="none" stroke="#FFFFFF" stroke-width="17" stroke-miterlimit="10" x1="988" y1="615" x2="988" y2="645.5"/>
+		<line id="joinUs8" fill="none" stroke="#FFFFFF" stroke-width="17" stroke-miterlimit="10" x1="988" y1="469.5" x2="988" y2="595.5"/>
+		<path id="joinUs7" fill="none" stroke="#FFFFFF" stroke-width="17" stroke-miterlimit="10" d="M853.5,626c51.5,23.5,82,5.5,82-19
+		c0-29.5-73-18.5-73-63c0-20.5,39-35,75.5-17"/>
+		<path id="joinUs6" fill="none" stroke="#FFFFFF" stroke-width="17" stroke-miterlimit="10" d="M809.5,512.5V642v-31.5C770.5,652,720,638,720,606
+		v-93.5"/>
+		<path id="joinUs5" fill="none" stroke="#FFFFFF" stroke-width="17" stroke-miterlimit="10" d="M510.5,642.5V513v32.5c52-48.7,89.5-19,89.5,2v97"
+		/>
+		<line id="joinUs4" fill="none" stroke="#FFFFFF" stroke-width="17" stroke-miterlimit="10" x1="448" y1="464.5" x2="448" y2="488"/>
+		<line id="joinUs3" fill="none" stroke="#FFFFFF" stroke-width="17" stroke-miterlimit="10" x1="448" y1="512.5" x2="448" y2="642.6"/>
+		<path id="joinUs2" fill="none" stroke="#FFFFFF" stroke-width="17" stroke-miterlimit="10" d="M342.3,518.3c-63.8,0-62.8,118.2,3.7,118.2
+			C411,636.5,415,518.3,342.3,518.3z"/>
+		<path id="joinUs1" fill="none" stroke="#FFFFFF" stroke-width="17" stroke-miterlimit="10" d="M245,469v180c0,17-12.7,41-50.7,29.4"/>
+	</g>
+</svg>
+
 </div>
+
+<h2>Finding Our Participants</h2>
 
 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque a vehicula ante. Sed pellentesque, lectus et faucibus elementum, erat erat vestibulum risus, vitae sollicitudin mauris turpis sed magna. In hac habitasse platea dictumst. Morbi dapibus sed lectus non efficitur. Suspendisse potenti. Pellentesque iaculis mattis justo, id finibus nibh suscipit non. Duis purus ipsum, blandit fringilla interdum et, faucibus ac mauris. Curabitur maximus consequat nisi, in tristique ligula eleifend eu. Maecenas id lacus quis erat vestibulum sodales. Aenean lectus mi, eleifend ac orci at, vulputate aliquet lectus. In hac habitasse platea dictumst. Ut rutrum fringilla velit ut sodales. Aenean ligula ligula, dapibus nec pulvinar ultrices, convallis elementum turpis.</p>
